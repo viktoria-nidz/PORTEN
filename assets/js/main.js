213cb3c1fda57e75dddafae1a3b06205fd580fde
“Vikroriiagit config --global user email iraruslan1035@gmail.comgit config --global core.editor code <iraruslan1035@gmail.com>
2022-10-10 20:20:35 +0300

    done version

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+new Swiper(".swiper-container", {
+  grabCursor: true,
+  //   hashNavigation: {
+  //     watchState: true,
+  //   },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  mousewheel: {
+    sensitivity: 1,
+    eventsTarget: ".swiper-container",
+  },
+  slidesPerView: 3,
+  spaceBetween: 50,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 25,
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+
+    1432: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+  //   loop: true,
+});
